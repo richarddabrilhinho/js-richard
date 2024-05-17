5ae28d8fdbb523abbfc794a011cb12637ad3fa07
richarddabrilhinho <00001078050235sp@al.educacao.sp.gov.br>
2024-05-17 10:52:59 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,13 @@
 function setup() {
-    createCanvas(400, 400);
-    background("black");
-  }
+  createCanvas(1000, 1000);
+  background("black");
+}
+
+function draw() {
+stroke ("blue");
+  fill ("red");
   
-  function draw() {
-  stroke ("blue");
-    fill ("red");
-    
-    if(mouseIsPressed){
-      rect(mouseX, mouseY, 20,35);
-    }
+  if(mouseIsPressed){
+    rect(mouseX, mouseY, 20,35);
   }
+}
